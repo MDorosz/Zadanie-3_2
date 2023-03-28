fb7b55d18257b15f521ec3062c3bfd7583110bce
MAteusz Dorosz <126687548+MDorosz@users.noreply.github.com>
2023-03-28 16:57:57 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+      Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Podaj tekst: ");
+        String tekst = scanner.nextLine();
+        
+        System.out.print("Wybierz opcję konwersji (1 - małe litery na duże, 2 - duże litery na małe): ");
+        int opcja = scanner.nextInt();
+        
+        String konwersja = "";
+        for (int i = 0; i < tekst.length(); i++) {
+            char c = tekst.charAt(i);
+            if (opcja == 1 && Character.isLowerCase(c)) {
+                c = Character.toUpperCase(c);
+            } else if (opcja == 2 && Character.isUpperCase(c)) {
+                c = Character.toLowerCase(c);
+            }
+            konwersja += c;
+        }
+        
+        System.out.println("Tekst po konwersji: " + konwersja);
+    }
+}
